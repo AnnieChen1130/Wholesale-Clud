edad2045b11a36d5c0231fa94422a6d959f623a4
Lihua Li <reven70777@gmail.com>
2017-03-31 23:04:17 -0700

    Update club.cpp

--- src/club.cpp
+++ src/club.cpp
@@ -1,12 +1,80 @@
 #include "club.h"
+#include "item.h"
 
 Club::Club()
 {
 
 }
 
-void club::printSortedPurchase()
+void Club::printSortedPurchase()
 {
   
 }
 
+int Club::getQuantity(Item item)
+{
+  
+}
+   
+void Club::printTotal(Item item)
+{
+  
+}
+  
+void Club::printQuantityByName(Item item)
+{
+  
+}
+
+void Club::printSortedQuantity(Item item)
+{
+  
+}
+  
+void Club::printRebate(Wholesale member)
+{
+  
+}
+
+void Club::printSortedIdByRebate()
+{
+  
+}
+
+double Club::getMembershipDue()
+{
+  
+}
+
+void Club::printSortedMembership()
+{
+  
+}
+
+
+void Club::printExpiredMembers(int month)
+{
+  
+}
+
+void Club::addTo(int month, int day, Wholesale member) //NOT CORRECT
+{
+  
+}
+  
+void CLub::deleteAt(int month, int day, int index)//NOT CORRECT
+{
+  
+}
+void Club::determineMembership()
+{
+  
+}
+void Club::getFile(string file)
+{
+  
+}
+void Club::writeTo(string report)
+{
+  
+}
